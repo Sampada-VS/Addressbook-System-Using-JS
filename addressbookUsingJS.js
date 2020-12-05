55824429da38aa7093bd851a2c2638519b36de79
Sampada-VS <samssv1@gmail.com>
2020-12-05 20:50:20 +0530

    Added: function to sort persons by name

--- addressbookUsingJS.js
+++ addressbookUsingJS.js
@@ -186,7 +186,7 @@ function countByState(){
 
 do{
     var choice=Number(prompt("Enter option : 1.Add New contact 2.Update Contact 3.Display all contacts 4.Delete Contact 5.Total Contacts 6.Search Person By City 7.Search Person By State 8.View Persons by City 9.View Persons by State "
-    +"10. Count by city 11. Count by state 12.Exit== "));
+    +"10. Count by city 11. Count by state 12.Sort By name 13.Exit== "));
     switch(choice){
         case 1: let fName=prompt("Enter First Name to add contact :");
                 let found=addressbookData.filter(contact => fName.toLowerCase() === contact.firstName.toLowerCase())
@@ -217,9 +217,12 @@ do{
         case 10:    countByCity();
                     break;    
         case 11:    countByState();
-                    break;         
-        case 12:    console.log("You exit the program.");
+                    break;   
+        case 12:    let sortByName=addressbookData.sort((a, b) => a.firstName.localeCompare(b.firstName));
+                    console.log(sortByName.toString());
+                    break;
+        case 13:    console.log("You exit the program.");
                     break;
         default:    console.log("Wrong choice.");
     }
-}while(choice != 12);
+}while(choice != 13);
